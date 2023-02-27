d60b889eb72639ffa58ee3214d644224af648672
Ameer Ogirimah <ameerogirimah@gmail.com>
2023-02-27 07:03:29 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _strlen - Returns the length of a string
+  *
+  * @s: String whose lenght is to be returned
+  * Return: Always int.
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+	char j;
+
+	do {
+		j = *(s + i);
+		i++;
+	}while (j != '\0');
+	return (i - 1);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 
 0-reset_to_98.c
 1-swap.c
+2-strlen.c
