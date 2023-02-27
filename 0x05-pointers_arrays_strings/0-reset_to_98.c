ea76a81aa9cca836612fe786ef325bb5770dcbc0
Ameer Ogirimah <ameerogirimah@gmail.com>
2023-02-27 06:10:52 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Reset variable n to 98
+  *
+  * @n: pointer to memory location of variable to reset
+  * Return: Always void
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
