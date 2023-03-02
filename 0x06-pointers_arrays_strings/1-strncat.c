3c2eaad60d839b52167f2249cb9eef00cff1d1aa
Ameer Ogirimah <ameerogirimah@gmail.com>
2023-03-02 06:38:33 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,11 @@
 char *_strncat(char *dest, char *src, int n)
 	{
 	int i, j, k, l;
-	char s[20000];
+	char s[1000000];
 
 	i = strlen(dest);
 	j = strlen(src);
+
 	l = 0;
 	for (k = 0; k < i; k++)
 	{
