48b943d6d33cfad8439d9c24c5e786ef284647de
Ameer Ogirimah <ameerogirimah@gmail.com>
2023-03-02 18:55:23 -0800

    Correct 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, k;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		k = s1[i] - s2[i];
 		if (k > 0)
